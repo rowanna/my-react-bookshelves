b2983760069ad303f259c4938264f50ba88c22cb
rowanna <rowankimna@gmail.com>
2025-01-21 19:31:55 +0900

    feat: 고객센터 페이지 적용

--- src/assets/components/Layout/Layout.jsx
+++ src/assets/components/Layout/Layout.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Layout = ({ children }) => {
   return (
     <section style={{ width: 440 }}>
       <div style={{ backgroundColor: "#888" }}>Header</div>
       {children}
-      <div style={{ backgroundColor: "#888" }}>Footer 고객센터</div>
+      <div style={{ backgroundColor: "#888" }}>
+        Footer
+        <Link to="/customer">고객센터</Link>
+      </div>
     </section>
   );
 };
--- src/assets/pages/CustomerPage.jsx
+++ src/assets/pages/CustomerPage.jsx
@@ -1,5 +1,17 @@
+import Container from "../components/Layout/Container";
+import Layout from "../components/Layout/Layout";
+
 const CustomerPage = () => {
-  return <>CustomerPage</>;
+  return (
+    <>
+      <Layout>
+        <Container title="고객센터">
+          <p>010-0000-0000</p>
+          <p>cs@example.com</p>
+        </Container>
+      </Layout>
+    </>
+  );
 };
 
 export default CustomerPage;
